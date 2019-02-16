309c7e59fca6b7408b27ff7005e4180351e1829f
P. Vamsi Krishna <vsmartdisk@gmail.com>
2019-02-17 00:05:19 +0530

    commit 5

--- app/src/main/java/com/agritech/empmanager/fragments/DashboardFragment.java
+++ app/src/main/java/com/agritech/empmanager/fragments/DashboardFragment.java
@@ -4,13 +4,16 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.agritech.empmanager.R;
 import com.agritech.empmanager.databinding.FragmentDashbordBinding;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -26,15 +29,26 @@ public class DashboardFragment extends Fragment {
 
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashbord, container, false);
 
+        //for create home button
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(binding.toolbar);
+
+        activity.setTitle("Dash board");
+
         return binding.getRoot();
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onDashboardFragmentInteraction(uri);
@@ -58,6 +72,14 @@ public class DashboardFragment extends Fragment {
         mListener = null;
     }
 
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.dashboard_menu,menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
     public interface OnDashboardFragmentInteractionListener {
         // TODO: Update argument type and name
